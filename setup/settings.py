6b4b19872ce591c86708c4385571cded055d8466
macbook air <macbookair@MacBook-Air-de-macbook.local>
2025-06-04 21:59:17 -0300

    alterações

--- setup/settings.py
+++ setup/settings.py
@@ -143,21 +143,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # <-- Adicione esta linha
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-CKEDITOR_UPLOAD_PATH = "uploads/"
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+CKEDITOR_UPLOAD_PATH = "uploads/"
+
+# Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
-
-
 # settings.py
-
-LOGIN_REDIRECT_URL = 'todos:home'  # Nome da URL da página inicial
+LOGIN_REDIRECT_URL = 'todos:home'
 LOGOUT_REDIRECT_URL = '/'
