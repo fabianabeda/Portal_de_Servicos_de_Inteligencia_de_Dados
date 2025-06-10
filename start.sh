fb43f4d034a7aa1bb2bbfb7097d2c836c04747ca
fabianabeda <fabiana.beda@gmail.com>
2025-06-09 21:00:47 -0300

    Corrige caminho do módulo WSGI para setup

--- start.sh
+++ start.sh
@@ -7,4 +7,5 @@ echo "🧹 Coletando arquivos estáticos..."
 python manage.py collectstatic --noinput
 
 echo "🚀 Iniciando o servidor Gunicorn..."
-gunicorn Portal_de_Servicos_de_Inteligencia_de_Dados.wsgi:application --bind 0.0.0.0:$PORT
+ggunicorn setup.wsgi:application --bind 0.0.0.0:$PORT
+
